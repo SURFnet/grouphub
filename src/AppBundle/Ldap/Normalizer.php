6c1a2becf2370fa122496806f7f7370e1c3a8c90
Lucas van Lierop <lucas@vanlierop.org>
2016-11-28 09:26:33 +0000

    Make sure name has no id

--- src/AppBundle/Ldap/Normalizer.php
+++ src/AppBundle/Ldap/Normalizer.php
@@ -125,6 +125,10 @@ class Normalizer
     {
         $mapping = $this->mapping['group'];
 
+        $extractNameFromCn = function ($cn) {
+            return substr($cn, 0, strrpos($cn, '_'));
+        };
+
         $result = [];
         for ($i = 0; $i < $groups['count']; $i++) {
             $group = $groups[$i];
@@ -132,7 +136,7 @@ class Normalizer
             $result[] = new Group(
                 null,
                 $group['dn'],
-                $group['cn'][0],
+                $extractNameFromCn($group['cn'][0]),
                 isset($group[$mapping['description']][0]) ? $group[$mapping['description']][0] : ''
             );
         }
